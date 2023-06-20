359ab57f96f7e16bfdfee4bce4233569e0750926
unpeko <132242919+unpeko@users.noreply.github.com>
2023-06-20 09:02:25 +0000

    made sequence 1 - (greeting) and statement generator

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-test2
+def statement_generator(statement, decoration) :
+
+  sides = decoration 
+
+  statement = "{} {} {}".format(sides, statement, sides)
+  top_bottom = decoration * len(statement)
+
+  print(top_bottom)
+  print(statement)
+  print(top_bottom)
+
+  return ""
+  
+statement_generator("Tēnā koe!","+")
+statement_generator("Welcome to Fraser's school canteen!","-")
+name = input ("What is your name? ")
+print()
+
+if name.isalpha() == False:
+  print("Sorry we really need to know your name to help us process this order.")
+  name = input("What is your name? ")
+  print()
+
+if name.isalpha():
+  welcoming = "Kia Ora {v}, welcome to the Fraser High School canteen! "
+  print(welcoming.format(v=name))
