0c97ff7a5883236a0d2a2f79b828c5f2da891279
unpeko <132242919+unpeko@users.noreply.github.com>
2023-06-30 00:15:05 +0000

    Made sequence 2 (menu yes_no )

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def statement_generator(statement, decoration) :
+# Functions go here...
 
-  sides = decoration 
+
+def statement_generator(statement, decoration):
+
+  sides = decoration
 
   statement = "{} {} {}".format(sides, statement, sides)
   top_bottom = decoration * len(statement)
@@ -10,17 +13,60 @@ def statement_generator(statement, decoration) :
   print(top_bottom)
 
   return ""
-  
-statement_generator("Tēnā koe!","+")
-statement_generator("Welcome to Fraser's school canteen!","-")
-name = input ("What is your name? ")
+
+
+def yes_no(question):
+  valid = False
+  while not valid:
+    response = input(question).lower()
+
+    if response == "yes" or response == "y":
+      response = "yes"
+      return response
+
+    elif response == "no" or response == "n":
+      response = "no"
+      return response
+
+    else:
+      print("Please answer yes / no")
+
+
+# Main routine goes here...
+
+# greeting
+
+statement_generator("Tēnā koe!", "+")
+statement_generator("Welcome to Fraser's school canteen!", "-")
+name = input("What is your name? ")
 print()
 
+# name checker (secqunce 1)
 if name.isalpha() == False:
-  print("Sorry we really need to know your name to help us process this order.")
+  print(
+    "Sorry, we really need to know your name to help us process this order.")
   name = input("What is your name? ")
   print()
 
 if name.isalpha():
   welcoming = "Kia Ora {v}, welcome to the Fraser High School canteen! "
   print(welcoming.format(v=name))
+  print()
+
+# menu yes_no (sequnce 2)
+greeting = ("{v}, would you like to see our menu? ")
+userinput = yes_no(greeting.format(v=name))
+print()
+
+if userinput == "yes":
+  print(
+    " On the special menu today we have the Pie for $4.50 and the Burger for $7.89."
+  )
+  menu = (
+    "{v} what would you like to order?   Please enter either a Pie or a Burger. "
+  )
+  print(menu.format(v=name))
+
+elif userinput == "no":
+  thankyou = ("Thank you {v} for visiting us at Fraser High School Canteen.")
+  print(thankyou.format(v=name))
