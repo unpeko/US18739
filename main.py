8d386aa1ed3b0dd42cb5f488aa58e535bb70bb69
unpeko <132242919+unpeko@users.noreply.github.com>
2023-07-18 22:15:04 +0000

    got rid of yes no def as it is not needed

--- main.py
+++ main.py
@@ -14,24 +14,6 @@ def statement_generator(statement, decoration):
 
   return ""
 
-
-def yes_no(question):
-  valid = False
-  while not valid:
-    response = input(question).lower()
-
-    if response == "yes" or response == "y":
-      response = "yes"
-      return response
-
-    elif response == "no" or response == "n":
-      response = "no"
-      return response
-
-    else:
-      print("Please answer yes / no")
-
-
 # Main routine goes here...
 
 # greeting
